e3e642ff2ee1d8c48b7d670492020e9cec1e908b
Abhinav Rana <63769307+AbhinavJRana@users.noreply.github.com>
2020-09-12 20:50:56 +0530

    Add files via upload
    Use test.txt To change the audio content you can add any number of sentences.
    In this I imported gtts (Google Text To Speech Module) You can easily install this in python using pip.
    You can also change audio language by changing  "language variable".

---
+++ textspeech.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+import os
+
+fh =open("test.txt","r")
+myText=fh.read().replace("\n"," ")
+
+language='en'
+
+output=gTTS(text=myText,lang=language,slow=True)
+
+output.save("output.mp3")
+fh.close()
+os.system("start output.mp3")
